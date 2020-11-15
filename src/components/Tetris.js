3648a5130d4a576ccf2bc3210a4dc6efe6da34c4
Ingrid Pruszynska <pruszynskaingrid@outlook.com>
2020-11-15 13:47:33 +0100

    Styling components

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,22 +1,27 @@
 import React from 'react'
-import {createStage} from '../gameHelpers'
+
+import { createStage } from '../gameHelpers'
+import { StyledTetrisWrapper, StyledTetris } from './styles/StyledTetris'
+
 import Stage from './Stage'
 import Display from './Display'
 import StartButton from './StartButton'
 
 const Tetris = ({ callback }) => {
   return (
-    <div>
-      <Stage stage={createStage()} />
-      <aside>
+    <StyledTetrisWrapper>
+      <StyledTetris>
+        <Stage stage={createStage()} />
+        <aside>
           <div>
             <Display text='Score'/>
             <Display text='Rows'/>
             <Display text='Level'/>
           </div>
           <StartButton />
-      </aside>
-    </div>
+        </aside>
+      </StyledTetris>
+    </StyledTetrisWrapper>
   )
 }
 
